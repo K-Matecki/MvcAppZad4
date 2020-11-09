82c50f2d63d2cb9a5b0cc2344f50192bf80b539b
K-Matecki <Kamil.wojciech.matecki@gmail.com>
2020-11-09 19:40:25 +0100

    Zadanie nr 4 na przedmiot programowanie zaawansowane

---
+++ mvc/Models/Bill.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc.Models
+{
+    public class Bill
+    {
+        public string Name { get; set; }
+        public float Value { get; set; }
+        public DateTime BillDate { get; set; }
+        public bool PaymentStatus { get; set; }
+        private static Random rnd = new Random();
+
+        public Bill()
+        {
+            Name = string.Empty;
+            Value = 0f;
+            BillDate = DateTime.Today;
+            PaymentStatus = false;
+        }
+        public static List<Bill> Examples()
+        {
+            return Enumerable.Range(0, 100).Select(x => new Bill { Name = $"{RandomName()}'s Bill", Value = RandomValue(), BillDate = RandomDate(), PaymentStatus = RandomBool() }).ToList();
+        }
+
+
+        private static string RandomName()
+        {
+            List<string> NameList = new List<string> { "Henrietta", "Nicolasa", "Tamala", "Francoise", "Burt", "Darline", "Nieves", "Elden", "Arcelia", "Mitch", "Rosanne", "Norah", "Eustolia", "Lilla" };
+
+            return NameList.OrderBy(x => rnd.Next(0, NameList.Count)).First();
+        }
+
+        private static bool RandomBool()
+        {
+            return rnd.Next() % 2 == 0 ? true : false;
+        }
+
+        private static DateTime RandomDate()
+        {
+            int range = 365;
+            DateTime randomDate = DateTime.Today.AddDays(rnd.Next(range));
+            return randomDate;
+        }
+        private static float RandomValue()
+        {
+            return rnd.Next(0, 1000);
+        }
+
+    }
+}
---
+++ mvc/Models/Budget.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc.Models
+{
+    public class Budget
+    {
+        public static List<Bill> BillList {get; set;}
+
+
+        public static void Examplse()
+        {
+            BillList = Bill.Examples();
+        }
+
+    }
+}
